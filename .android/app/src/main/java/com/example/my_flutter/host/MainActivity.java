33ba6026ac423994a76556ae7fe5fc1898fa28ff
BM00786 <biswa.bm60@gmail.com>
2022-06-21 16:21:27 +0530

    test

---
+++ .android/app/src/main/java/com/example/my_flutter/host/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.my_flutter.host;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+       GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+    }
